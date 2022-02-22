2a84aa1b11b1a73f9f551e407a58216388532c60
lijunliang <lijunliang@joyy.com>
2022-02-22 22:34:07 +0800

    git test 2

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('111');
+  console.log('11');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
